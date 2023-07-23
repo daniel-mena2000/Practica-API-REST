af9abd1579cc309c0b2c4890b65b6106294e6e81
daniel mena <dmena044@gmail.com>
2023-07-23 15:33:28 -0600

    api peliculas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,57 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers:{
+        "Content-Type": "application/json;charset=utf-8"
+    },
+    params:{
+        'api_key': API_KEY,
+    }
+});
+
+async function getTrendingMoviesPreview(){
+    const { data } = await api('trending/movie/day');
+    const movies = data.results;
+
+    movies.forEach(element => {
+        const trendingPreviewMoviesConteiner = document.querySelector("#trendingPreview .trendingPreview-movieList ")
+
+        const movieConteiner = document.createElement("div");
+        movieConteiner.classList.add('movie-container');
+
+        const movieImg = document.createElement("img");
+        movieImg.classList.add("movie-img");
+        movieImg.setAttribute("alt", element.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + element.poster_path);
+
+        movieConteiner.appendChild(movieImg);
+        trendingPreviewMoviesConteiner.appendChild(movieConteiner)
+    });
+}
+
+
+//API que apunta a "Movie Lista" para las categorias
+async function getCategoriesPreview(){
+    const { data } = await api('/genre/movie/list');
+    const generos = data.genres
+    console.log(generos);
+
+    generos.forEach(element => {
+        const categoriesPreviewMoviesConteiner = document.querySelector("#categoriesPreview .categoriesPreview-list ")
+
+        const div = document.createElement("div");
+        div.classList.add("category-container");
+        const h3 = document.createElement("h3");
+        h3.setAttribute("id",'id' + element.id)
+        h3.classList.add("category-title");
+        h3.textContent = element.name
+
+        div.appendChild(h3);
+        categoriesPreviewMoviesConteiner.appendChild(div)
 
 
 
+    });
+}
+
+getTrendingMoviesPreview()
+getCategoriesPreview()
